7294e6ddb88bdff586b8c6fb54ef772797580ee2
Piotr Szurlej <piteeroo@yahoo.com>
2016-02-23 20:55:33 +0100

    Create Main.java

---
+++ com.TrainingDiary.Launching/Main.java
@@ -0,0 +1,12 @@
+package com.TrainingDiary.Launching;
+
+import com.TrainingDiary.Calculations.GeneralCalculation;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		GeneralCalculation.runProgram();
+
+	}
+}
