d069b1f4a6b400bc5a49323521b17c5037334772
Piotr Szurlej <piteeroo@yahoo.com>
2016-02-23 20:52:07 +0100

    Create GeneralCalculation.java

---
+++ com.TrainingDiary.Calculations/GeneralCalculation.java
@@ -0,0 +1,11 @@
+package com.TrainingDiary.Calculations;
+
+public class GeneralCalculation {
+
+	public static void runProgram() throws Exception {
+		Menu menu = new Menu();
+		menu.menu();
+		menu.repeatMenu();
+	}
+
+}
